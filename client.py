877093e7ed4d6fc4b56a742a1a563ed741de96ed
LEEHYOJIN22 <leehyojin489@naver.com>
2023-10-19 23:57:32 +0900

    test commit message

--- client.py
+++ client.py
@@ -9,16 +9,18 @@ class VideoChatClient:
         self.ui = VideoChatUI(tk.Tk(), "화상 채팅 클라이언트")
         self.ui.on_send_message = self.send_message_to_server
         self.clients = []
-
+#소켓초기화
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(('localhost', 2323))
-
+#웹캠초기화
+        self.cap = cv2.VideoCapture(0)
+#웹캠이미지표시시작
         self.show.frame()
-
+#메시지수신스레드시작
         self.receive_thread = threading.Thread(target=self.receive_message)
         self.receive_thread.daemon = True
         self.receive_thread.start()
-
+#클라이언트GUI시작
         tk.mainloop()
 
     def show_frame(self):
--- server.py
+++ server.py
@@ -9,17 +9,17 @@ class VideoChatServer:
         self.ui = VideoChatUI(tk.Tk(), "화상 채팅 서버")
         self.ui.on_send_message = self.send_message_to_clients
         self.clients = []
-
+#웹캠초기화
         self.cap = cv2.VideoCapture(0)
-
+#소켓초기화
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind(('0.0.0.0', 2323))
         self.server_socket.listen(5)
-
+#웹캠영상전송스레드시작
         self.webcam_thread = threading.Thread(target=self.send_webcam)
         self.webcam_thread.daemon = True
         self.webcam_thread.start()
-
+#클라이언트연결을처리하는스레드시작
         self.receive_thread = threading.Thread(target=self.receive_clients)
         self.receive_thread.daemon = True
         self.receive_thread.start()
